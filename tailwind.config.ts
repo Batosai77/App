a60f39cd2398382ef759ac591b5e0bdb18686130
Batosai77 <https://Batosai77@github.com>
2023-10-25 14:44:31 +0700

    add particles background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        'textColor': 'text 5s ease infnite'
       },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%':{
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          } 
+        }
+      } 
     },
   },
   plugins: [],
