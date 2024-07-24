438f294f753038f50665d5c2022f345177c55873
Batosai77 <yogirahmansetia@gmail.com>
2024-07-24 21:56:48 +0700

    chore: comment seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,27 +3,27 @@ import { hash } from "bcrypt";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const password = await hash("password", 12);
-  const user = await prisma.user.upsert({
-    where: { email: "superadmin@gmail.com" },
-    update: {
-      email: "superadmin@gmail.com",
-      name: "Admin",
-      password: password,
-    },
-    create: {
-      email: "superadmin@gmail.com",
-      name: "Admin",
-      password: password,
-    },
-  });
-  console.log({ user });
-}
-main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// async function main() {
+//   const password = await hash("password", 12);
+//   const user = await prisma.user.upsert({
+//     where: { email: "superadmin@gmail.com" },
+//     update: {
+//       email: "superadmin@gmail.com",
+//       name: "Admin",
+//       password: password,
+//     },
+//     create: {
+//       email: "superadmin@gmail.com",
+//       name: "Admin",
+//       password: password,
+//     },
+//   });
+//   console.log({ user });
+// }
+// main()
+//   .then(() => prisma.$disconnect())
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
